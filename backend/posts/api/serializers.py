2780c57869844b7247ce56ae7b30f58b8e279264
Sanj420 <138890201+Sanj420@users.noreply.github.com>
2024-11-04 11:55:19 +0530

    Gay

--- backend/posts/api/serializers.py
+++ backend/posts/api/serializers.py
@@ -1,7 +1,37 @@
 from rest_framework.serializers import ModelSerializer
-from ..models import Post
+from ..models import Post,Theater, Show, Performance, Ticket, Customer, Booking
 
 class PostSerializer(ModelSerializer):
     class Meta:
         model = Post
         fields = ('title' , 'body')
+
+class TheaterSerializer(ModelSerializer):
+    class Meta:
+        model = Theater
+        fields = ('name' , 'location','seating_capacity')
+
+class ShowSerializer(ModelSerializer):
+    class Meta:
+        model = Show
+        fields = ('' , '')
+
+class PerformanceSerializer(ModelSerializer):
+    class Meta:
+        model = Performance
+        fields = ('' , '')
+
+class TicketSerializer(ModelSerializer):
+    class Meta:
+        model = Ticket
+        fields = ('' , '')
+
+class CustomerSerializer(ModelSerializer):
+    class Meta:
+        model = Customer
+        fields = ('' , '')
+
+class BookingSerializer(ModelSerializer):
+    class Meta:
+        model = Booking
+        fields = ('' , '')
--- backend/posts/models.py
+++ backend/posts/models.py
@@ -8,3 +8,42 @@ class Post(models.Model):
 
     def _str_(self):
         return f"Post: {self.title}"
+    
+class Theater(models.Model):
+    name = models.CharField(max_length=100)
+    location = models.CharField(max_length=255)
+    seating_capacity = models.IntegerField()
+
+
+class Show(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    duration_minutes = models.IntegerField()
+    genre = models.CharField(max_length=50, blank=True, null=True)
+    age_rating = models.CharField(max_length=10, blank=True, null=True)
+
+
+class Performance(models.Model):
+    show = models.ForeignKey(Show, on_delete=models.CASCADE)
+    theater = models.ForeignKey(Theater, on_delete=models.CASCADE)
+    date_time = models.DateTimeField()
+    available_seats = models.IntegerField()
+
+class Ticket(models.Model):
+    performance = models.ForeignKey(Performance, on_delete=models.CASCADE)
+    seat_number = models.CharField(max_length=10)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    is_booked = models.BooleanField(default=False)
+
+class Customer(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField(unique=True)
+    phone_number = models.CharField(max_length=15, blank=True, null=True)
+
+class Booking(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    performance = models.ForeignKey(Performance, on_delete=models.CASCADE)
+    tickets = models.ManyToManyField(Ticket)
+    booking_date = models.DateTimeField(auto_now_add=True)
+    total_price = models.DecimalField(max_digits=6, decimal_places=2)
